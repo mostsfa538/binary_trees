7df069fcc961ebd88f3142c984585d5389e3f403
mostsfa538 <mostafa51moktar@gmail.com>
2024-03-25 23:17:28 +0200

    get the height of specific node

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: The height of the tree, or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_left = 1, height_right = 1;
+
+	if (!tree || (!tree->left && !tree->right))
+	{
+		return (0);
+	}
+	height_left = binary_tree_height(tree->left);
+	height_right = binary_tree_height(tree->right);
+
+	if (height_left > height_right)
+		return (height_left + 1);
+	else
+		return (height_right + 1);
+}
