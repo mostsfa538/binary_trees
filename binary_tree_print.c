8e09ec0cafc29f13fe3a3d5d8b58dd3b25e3c18e
mostsfa538 <mostafa51moktar@gmail.com>
2024-03-25 17:34:31 +0200

    create and update

--- binary_tree_print.c
+++ binary_tree_print.c
@@ -1,8 +1,9 @@
+#include "binary_trees.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "binary_trees.h"
 
+/* Original code from http://stackoverflow.com/a/13755911/5184480 */
 
 /**
  * print_t - Stores recursively each level in an array of strings
