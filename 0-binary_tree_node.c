0df78f3f325ec3b3a837270a07a167ac7c6d0b1c
mostsfa538 <mostafa51moktar@gmail.com>
2024-03-25 21:52:56 +0200

    add new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,9 +13,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	if (!parent)
-		return (NULL);
-
 	new_node = malloc(sizeof(binary_tree_t));
 
 	if (!new_node)
@@ -23,8 +20,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	new_node->parent = parent;
 	new_node->n = value;
-	parent->left = NULL;
-	parent->right = NULL;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
 	return (new_node);
 }
