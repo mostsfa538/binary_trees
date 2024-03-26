83efc3a1dcd18d676d84c753206e8f18fcb59551
mostsfa538 <mostafa51moktar@gmail.com>
2024-03-26 08:43:11 +0200

    print the tree in preorder

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -13,10 +13,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (!tree || !func)
 		return;
 
-	if (tree)
-	{
-		func(tree->n);
-		binary_tree_preorder(tree->left, func);
-		binary_tree_preorder(tree->right, func);
-	}
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
