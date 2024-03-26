8948f3460c585930f59148821a606e5752a3ee6f
mostsfa538 <mostafa51moktar@gmail.com>
2024-03-26 10:37:56 +0200

    get the uncle node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_uncle - finds the uncle of a node
- * @ node: is a pointer to the node to find the uncle
+ * @node: is a pointer to the node to find the uncle
  *
  * Return:  pointer to the uncle node
  */
