6d133bb5b604b7687187c8ccbfcf6b41d4d81401
mostsfa538 <mostafa51moktar@gmail.com>
2024-03-26 10:27:01 +0200

    get the sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,7 +8,13 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (!node || !node->parent || !node->parent->right)
-        return (NULL);
-    return (node->parent->right);
+	if (!node || !node->parent)
+		return (NULL);
+	if (node->parent->left && node->parent->left != node)
+		return (node->parent->left);
+
+	if (node->parent->right && node->parent->right != node)
+		return (node->parent->right);
+
+	return (NULL);
 }
