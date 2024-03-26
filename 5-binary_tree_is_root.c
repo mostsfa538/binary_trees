bec39eff96d924018793dd920711f498f8f84ab4
mostsfa538 <mostafa51moktar@gmail.com>
2024-03-26 08:29:00 +0200

    check if mode is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -12,7 +12,8 @@ int binary_tree_is_root(const binary_tree_t *node)
 	if (!node)
 		return (0);
 
-	if (node->left && node->right)
+	if (!node->parent)
 		return (1);
-	return (0);
+	else
+		return (0);
 }
