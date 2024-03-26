43c3767ac40a219838e181bf60a965d038880a8f
mostsfa538 <mostafa51moktar@gmail.com>
2024-03-26 08:35:10 +0200

    print the tree in preorder

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -4,7 +4,6 @@
  * binary_tree_preorder - goes through a binary tree using pre-order traversal
  * @tree:  is a pointer to the root node of the tree to traverse
  * @fun: is a pointer to a function to call for each node. The value in the node
- * must be passed as a parameter to this function.
  *
  * Return: no thing
  */
